378cf332d08cbe191257976b90e4783d8bf040ad
666RED <hongchensee8@gmail.com>
2023-01-05 13:49:50 +0800

    added comment

--- Dijkstra.java
+++ Dijkstra.java
@@ -4,6 +4,7 @@ import java.util.List;
 public class Dijkstra {
 	static final int NUM_OF_STATIONS = 6;
 	
+	// find the minDistanceNode from all unvisited nodes
 	int minDistance(int pathArray[], Boolean visitedNodes[]) {
 		int min = Integer.MAX_VALUE;
 		int minIndex = -1;
@@ -17,7 +18,7 @@ public class Dijkstra {
 		return minIndex;
 	}
 	
-	void printMinPath(int pathArray[], ArrayList<List<String>> path) {
+	void printMinPath(int pathArray[], ArrayList<List<String>> path) { // just for the purpose of printing out result
 		System.out.println("Minimum distance from KL to each station\n");
 		System.out.println("From \t\t To \t\t Minimum Path");
 		System.out.println("---------------------------------------------");
@@ -51,34 +52,39 @@ public class Dijkstra {
 		int pathArray[] = new int[NUM_OF_STATIONS];
 		Boolean visitedNodes[] = new Boolean[NUM_OF_STATIONS];
 		
-		for(int i = 0; i < NUM_OF_STATIONS; i++) {
+		for(int i = 0; i < NUM_OF_STATIONS; i++) { // set all node as unvisited && all node has the minimum distance of infinity
 			pathArray[i] = Integer.MAX_VALUE;
 			visitedNodes[i] = false;
 		}
 		
-		pathArray[sourceNode] = 0;
+		pathArray[sourceNode] = 0; // set distance of KL as 0 because it is the origin
 		
 		for(int count = 0; count < NUM_OF_STATIONS; count++) {
-			int minDistanceNodeIndex = minDistance(pathArray, visitedNodes);
-			visitedNodes[minDistanceNodeIndex] = true;
+			int minDistanceNodeIndex = minDistance(pathArray, visitedNodes); // find the node with minimum distance
+			visitedNodes[minDistanceNodeIndex] = true; // set it as visited
 			
 			for(int v = 0; v < NUM_OF_STATIONS; v++) {
-				if(!visitedNodes[v] && graph[minDistanceNodeIndex][v] != 0 && pathArray[minDistanceNodeIndex] != Integer.MAX_VALUE && pathArray[minDistanceNodeIndex] + graph[minDistanceNodeIndex][v] < pathArray[v]) {
+				// if the node hasn't been visiting
+				// the distance of minimum node is not infinity
+				// the distance between the minimum node and the connected node is not 0 (they are connected)
+				// this distance is less than the original distance of the connected node 
+				// update the minimum distance (distance of minimum node + distance between it and connected node)
+				if(!visitedNodes[v] && pathArray[minDistanceNodeIndex] != Integer.MAX_VALUE && graph[minDistanceNodeIndex][v] != 0 && pathArray[minDistanceNodeIndex] + graph[minDistanceNodeIndex][v] < pathArray[v]) {
 					pathArray[v] = pathArray[minDistanceNodeIndex] + graph[minDistanceNodeIndex][v];
 					if(path.get(v).size() != 1) {// to update the minimum path
 						int sizeOfArray = path.get(v).size();
 						for(int i = 1; i < sizeOfArray; i++) {
-							path.get(v).remove(1);
+							path.get(v).remove(1); // remove the original path, but do not remove the reference station node
 						}
 					}
-					if(path.get(minDistanceNodeIndex).size() == 1) {
+					if(path.get(minDistanceNodeIndex).size() == 1) { // if the minimum node has only itself
 						path.get(v).add(path.get(minDistanceNodeIndex).get(0));
 					}else {
 						for(int i = 1; i < path.get(minDistanceNodeIndex).size(); i++) {
-							path.get(v).add(path.get(minDistanceNodeIndex).get(i));
+							path.get(v).add(path.get(minDistanceNodeIndex).get(i)); // adding all station from minimum node
 						}
 					}
-					path.get(v).add(path.get(v).get(0));
+					path.get(v).add(path.get(v).get(0)); // add the reference station node at the back of the path
 				}
 			}
 		}
--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
+		// create a graph that consists all the distance between two stations
 		int graph[][] = new int [][]{{0, 20, 40, 0, 0, 0}, 
 						 			 {20, 0, 100, 0, 0, 70},
 						 			 {40, 100, 0, 30, 0, 0},
@@ -11,11 +12,17 @@ public class Main {
 						 			 {0, 0, 0, 180, 0 ,100},
 						 			 {0, 70, 0, 0, 100, 0}
 									};
+		// create a 2d AraryList to store the min path
 		ArrayList<List<String>> path = new ArrayList<List<String>>(6);
+		
+		// create ArrayList for each element in the 2d ArrayList
 		for(int i = 0; i < 6; i++) {
 			path.add(new ArrayList<String>());
 		}
+		
 		String stations[] = {"Kuala Lumpur", "Parit Raja", "Kajang", "Nusa", "Johor Bahru", "Singapore"};
+		
+		// Add reference station for each ArrayList
 		for(int i = 0; i < stations.length; i++) {
 			path.get(i).add(stations[i]);
 		}
